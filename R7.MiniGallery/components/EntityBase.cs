61341478d5a065781204056a10c3fe07b6094b20
roman-yagodin <roman.yagodin@gmail.com>
2014-07-18 12:43:06 +0400

    Added IAuditable interface and EntityBase class

---
+++ R7.MiniGallery/components/EntityBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DotNetNuke.Entities.Portals;
+using DotNetNuke.Entities.Users;
+using DotNetNuke.ComponentModel.DataAnnotations;
+
+namespace R7.MiniGallery
+{
+	public abstract class EntityBase : IAuditable
+	{
+		public EntityBase ()
+		{
+		}
+
+		#region IAuditable implementation
+
+		public int LastModifiedByUserID { get; set; }
+		public DateTime LastModifiedOnDate { get; set; }
+		public int CreatedByUserID { get; set; }
+		public DateTime CreatedOnDate { get; set; }
+
+		// REVIEW: Make CreatedOnDate a [ReadOnlyColumn]?
+
+		#endregion
+
+
+	}
+}
