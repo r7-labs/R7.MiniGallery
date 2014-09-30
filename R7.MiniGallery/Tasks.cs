4489717f447831befb1993e2240b6a17e7694877
roman-yagodin <roman.yagodin@gmail.com>
2014-09-30 11:57:28 +0400

    Move common code tasks to Tasks.cs, insert / update standard headers

---
+++ R7.MiniGallery/Tasks.cs
@@ -0,0 +1,8 @@
+﻿// TODO: Update imagePreview then new file in urlImage is selected
+// TODO: Shorter setting prefix, e.g. MiniGallery_ => r7mg_
+
+// REVIEW: How to display headers on images?
+// REVIEW: Simplify styleset names: MG_Default => mgDefault 
+// REVIEW: Don't use imagehandler if both width and height isn't set
+// REVIEW: Separate setting for image dimension units (same for width and height)
+// REVIEW: Display image title only on hover
--- R7.MiniGallery/components/MiniGallerySettings.cs
+++ R7.MiniGallery/components/MiniGallerySettings.cs
@@ -1,4 +1,28 @@
-// TODO: Shorter setting prefix, e.g. MiniGallery_ => r7mg_
+//
+// MiniGallerySettings.cs
+//
+// Author:
+//       Roman M. Yagodin <roman.yagodin@gmail.com>
+//
+// Copyright (c) 2014 
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 using System;
 using System.Web.UI.WebControls;
@@ -13,22 +37,38 @@ namespace R7.MiniGallery
 	/// </summary>
 	public partial class MiniGallerySettings : SettingsWrapper
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="R7.MiniGallery.MiniGallerySettings"/> class.
+		/// </summary>
+		/// <param name="module">Module.</param>
 		public MiniGallerySettings (IModuleControl module) : base (module)
 		{
 		}
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="R7.MiniGallery.MiniGallerySettings"/> class.
+		/// </summary>
+		/// <param name="module">Module.</param>
 		public MiniGallerySettings (ModuleInfo module) : base (module)
 		{
 		}
 
 		#region Module settings
 
+		/// <summary>
+		/// Gets or sets a value indicating whether this <see cref="R7.MiniGallery.MiniGallerySettings"/> use image handler.
+		/// </summary>
+		/// <value><c>true</c> if use image handler; otherwise, <c>false</c>.</value>
 		public bool UseImageHandler 
 		{
 			get { return ReadSetting<bool> ("MiniGallery_UseImageHandler", true); }
 			set { WriteModuleSetting<bool> ("MiniGallery_UseImageHandler", value); }
 		}
 		
+		/// <summary>
+		/// Gets or sets the image handler parameters.
+		/// </summary>
+		/// <value>The image handler parameters.</value>
 		public string ImageHandlerParams
 		{
 			// HACK: DNN SPs have a special handle of SettingValues starting with "fileid" 
@@ -47,18 +87,30 @@ namespace R7.MiniGallery
 			set { WriteTabModuleSetting<bool> ("MiniGallery_UseLightbox", value); }
 		}*/
 
+		/// <summary>
+		/// Gets or sets the type of the lightbox.
+		/// </summary>
+		/// <value>The type of the lightbox.</value>
 		public LightboxType LightboxType
 		{
 			get { return (LightboxType)Enum.Parse (typeof(LightboxType), ReadSetting<string> ("MiniGallery_LightboxType", LightboxType.LightBox.ToString())); }
 			set { WriteTabModuleSetting<string> ("MiniGallery_LightboxType", value.ToString()); }
 		}
 
+		/// <summary>
+		/// Gets or sets a value indicating whether this <see cref="R7.MiniGallery.MiniGallerySettings"/> use scrollbar.
+		/// </summary>
+		/// <value><c>true</c> if use scrollbar; otherwise, <c>false</c>.</value>
 		public bool UseScrollbar
 		{
 			get { return ReadSetting<bool> ("MiniGallery_UseScrollbar", true); }
 			set { WriteTabModuleSetting<bool> ("MiniGallery_UseScrollbar", value); }
 		}
 
+		/// <summary>
+		/// Gets or sets the style set.
+		/// </summary>
+		/// <value>The style set.</value>
 		public string StyleSet
 		{
 			get { return ReadSetting<string> ("MiniGallery_StyleSet", "Default"); }
@@ -75,66 +127,110 @@ namespace R7.MiniGallery
 			set { WriteTabModuleSetting<int> ("MiniGallery_MaxHeight", value); }
 		}
 
+		/// <summary>
+		/// Gets or sets the target.
+		/// </summary>
+		/// <value>The target.</value>
 		public string Target
 		{
 			get { return ReadSetting<string> ("MiniGallery_Target", "_blank"); }
 			set { WriteTabModuleSetting<string> ("MiniGallery_Target", value); }
 		}
 
+		/// <summary>
+		/// Gets or sets the width of the image.
+		/// </summary>
+		/// <value>The width of the image.</value>
 		public Unit ImageWidth
 		{
 			get { return Unit.Parse(ReadSetting<string> ("MiniGallery_ImageWidth", string.Empty)); }
 			set { WriteTabModuleSetting<string> ("MiniGallery_ImageWidth", value.ToString()); }
 		}
 
+		/// <summary>
+		/// Gets or sets the height of the image.
+		/// </summary>
+		/// <value>The height of the image.</value>
 		public Unit ImageHeight
 		{
 			get { return Unit.Parse(ReadSetting<string> ("MiniGallery_ImageHeight", string.Empty)); }
 			set { WriteTabModuleSetting<string> ("MiniGallery_ImageHeight", value.ToString()); }
 		}
 
+		/// <summary>
+		/// Gets or sets the width of the thumb.
+		/// </summary>
+		/// <value>The width of the thumb.</value>
 		public int ThumbWidth
 		{
 			get { return ReadSetting<int> ("MiniGallery_ThumbWidth", 240); }
 			set { WriteTabModuleSetting<int> ("MiniGallery_ImageWidth", value); }
 		}
 		
+		/// <summary>
+		/// Gets or sets the height of the thumb.
+		/// </summary>
+		/// <value>The height of the thumb.</value>
 		public int ThumbHeight
 		{
 			get { return ReadSetting<int> ("MiniGallery_ThumbHeight", Null.NullInteger); }
 			set { WriteTabModuleSetting<int> ("MiniGallery_ImageHeight", value); }
 		}		
 
+		/// <summary>
+		/// Gets or sets a value indicating whether this <see cref="R7.MiniGallery.MiniGallerySettings"/> show info.
+		/// </summary>
+		/// <value><c>true</c> if show info; otherwise, <c>false</c>.</value>
 		public bool ShowInfo
 		{
 			get { return ReadSetting<bool> ("MiniGallery_ShowInfo", false); }
 			set { WriteTabModuleSetting<bool> ("MiniGallery_ShowInfo", value); }
 		}
 
+		/// <summary>
+		/// Gets or sets a value indicating whether this <see cref="R7.MiniGallery.MiniGallerySettings"/> show titles.
+		/// </summary>
+		/// <value><c>true</c> if show titles; otherwise, <c>false</c>.</value>
 		public bool ShowTitles
 		{
 			get { return ReadSetting<bool> ("MiniGallery_ShowTitles", true); }
 			set { WriteTabModuleSetting<bool> ("MiniGallery_ShowTitles", value); }
 		}
 
+		/// <summary>
+		/// Gets or sets a value indicating whether this <see cref="R7.MiniGallery.MiniGallerySettings"/> expand columns.
+		/// </summary>
+		/// <value><c>true</c> if expand columns; otherwise, <c>false</c>.</value>
 		public bool ExpandColumns
 		{
 			get { return ReadSetting<bool> ("MiniGallery_ExpandColumns", false); }
 			set { WriteTabModuleSetting<bool> ("MiniGallery_ExpandColumns", value); }
 		}
 
+		/// <summary>
+		/// Gets or sets the columns.
+		/// </summary>
+		/// <value>The columns.</value>
 		public int Columns
 		{
 			get { return ReadSetting<int> ("MiniGallery_Columns", Null.NullInteger); }
 			set { WriteTabModuleSetting<int> ("MiniGallery_Columns", value); }
 		}
 		
+		/// <summary>
+		/// Gets or sets the number of records.
+		/// </summary>
+		/// <value>The number of records.</value>
 		public int NumberOfRecords
 		{
 			get { return ReadSetting<int> ("MiniGallery_NumberOfRecords", Null.NullInteger); }
 			set { WriteTabModuleSetting<int> ("MiniGallery_NumberOfRecords", value); }
 		}
 
+		/// <summary>
+		/// Gets or sets the sort order.
+		/// </summary>
+		/// <value>The sort order.</value>
 		public string SortOrder
 		{
 			get { return ReadSetting<string> ("MiniGallery_SortOrder", "SortIndex"); }
