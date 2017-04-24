4e7a8357c56c5086ee14635f484a324bc0456318
roman-yagodin <roman.yagodin@gmail.com>
2017-04-24 16:34:58 +0300

    Pass props to React component using data attributes

--- R7.MiniGallery/js/minigallery.jsx
+++ R7.MiniGallery/js/minigallery.jsx
@@ -11,11 +11,22 @@
     }
 }
 
-class MiniGallery extends React.Component {
+class BlueimpGallery extends React.Component {
     render() {
         return (
-            <div className="links MG_List">
-                {this.props.Images.map((img) => <MiniGalleryImage Src={img.ImageSrc} Alt={img.Alt} Title={img.Title} EditUrl={img.EditUrl} />)}
+            <div>
+                <div id={"lightbox-" + this.props.ModuleId} className="blueimp-gallery blueimp-gallery-controls">
+                    <div className="slides"></div>
+                    <h3 className="title"></h3>
+                    <a className="prev">&lt;</a>
+                    <a className="next">&gt;</a>
+                    <a className="close">&times;</a>
+                    <a className="play-pause"></a>
+                    <ol className="indicator"></ol>
+                </div>
+                <div className="links MG_List">
+                    {this.props.Images.map((img) => <MiniGalleryImage Src={img.ImageSrc} Alt={img.Alt} Title={img.Title} EditUrl={img.EditUrl} />)}
+                </div>
             </div>
         );
     }
