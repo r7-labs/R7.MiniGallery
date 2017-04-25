c0d77ecd782dc80390c548225cfd3794b10ffc65
roman-yagodin <roman.yagodin@gmail.com>
2017-04-25 12:13:05 +0300

    Move gallery initialization to JSX (still have some issues)

--- R7.MiniGallery/js/minigallery.jsx
+++ R7.MiniGallery/js/minigallery.jsx
@@ -15,7 +15,7 @@ class BlueimpGallery extends React.Component {
     render() {
         return (
             <div>
-                <div id={"lightbox-" + this.props.ModuleId} className="blueimp-gallery blueimp-gallery-controls">
+                <div className="blueimp-gallery blueimp-gallery-controls">
                     <div className="slides"></div>
                     <h3 className="title"></h3>
                     <a className="prev">&lt;</a>
@@ -24,10 +24,33 @@ class BlueimpGallery extends React.Component {
                     <a className="play-pause"></a>
                     <ol className="indicator"></ol>
                 </div>
-                <div className="links MG_List">
+                <div className="MG_List">
                     {this.props.Images.map((img) => <MiniGalleryImage Src={img.ImageSrc} Alt={img.Alt} Title={img.Title} EditUrl={img.EditUrl} />)}
                 </div>
             </div>
         );
     }
 }
+
+(function ($, window, document) {
+    $(() => {
+        $(".minigallery-inner").each ((i, m) => {
+            var moduleId = $(m).data ("module-id");
+            ReactDOM.render(
+                <BlueimpGallery ModuleId={moduleId} Images={$(m).data ("images")} />, m
+            );
+            var container = $(m).find(".blueimp-gallery").first();
+            var links = $(m).find("a.MG_Link");
+            $("a.MG_Link").click ((event) => {
+                var target = event.target || event.srcElement,
+                link = target.src ? target.parentNode : target,
+                options = {
+                    index: link,
+                    event: event,
+                    container: container
+                };
+                blueimp.Gallery(links, options);
+            });
+        });
+    });
+}) (jQuery, window, document);
