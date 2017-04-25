31099b76faf19bc6f2dbcfe56b1e3da0d9e32ddd
roman-yagodin <roman.yagodin@gmail.com>
2017-04-25 14:07:44 +0300

    Move blueimp-related parts to separate JSX file

---
+++ R7.MiniGallery/js/blueimp-lightbox.jsx
@@ -0,0 +1,47 @@
+class BlueimpLightbox extends React.Component {
+    render() {
+        return (
+            <div className="blueimp-gallery blueimp-gallery-controls">
+                <div className="slides"></div>
+                <h3 className="title"></h3>
+                <a className="prev">&lt;</a>
+                <a className="next">&gt;</a>
+                <a className="close">&times;</a>
+                <a className="play-pause"></a>
+                <ol className="indicator"></ol>
+            </div>
+        );
+    }
+}
+
+(function ($, window, document) {
+    $(() => {
+        ReactDOM.render(
+            <BlueimpLightbox />, $(".minigallery-lightbox").get(0)
+        );
+        var container = $(".minigallery-lightbox .blueimp-gallery").get(0);
+        $(".minigallery-inner").each ((i, m) => {
+            var moduleId = $(m).data ("module-id");
+            ReactDOM.render(
+                <MiniGallery
+                    ModuleId={moduleId}
+                    IsEditable={$(m).data ("is-editable")}
+                    EditIcon={$(m).data ("edit-icon")}
+                    Images={$(m).data ("images")}
+                />, m
+            );
+            $("a.MG_Link").click ((event) => {
+                var target = event.target || event.srcElement,
+                link = target.src ? target.parentNode : target,
+                options = {
+                    hidePageScrollbars: false,
+                    index: link,
+                    event: event,
+                    container: container
+                };
+                var links = $(target).closest(".minigallery-inner").find("a.MG_Link").get();
+                blueimp.Gallery(links, options);
+            });
+        });
+    });
+}) (jQuery, window, document);
--- R7.MiniGallery/js/minigallery.jsx
+++ R7.MiniGallery/js/minigallery.jsx
@@ -24,23 +24,7 @@
     }
 }
 
-class BlueimpGalleryLightbox extends React.Component {
-    render() {
-        return (
-            <div className="blueimp-gallery blueimp-gallery-controls">
-                <div className="slides"></div>
-                <h3 className="title"></h3>
-                <a className="prev">&lt;</a>
-                <a className="next">&gt;</a>
-                <a className="close">&times;</a>
-                <a className="play-pause"></a>
-                <ol className="indicator"></ol>
-            </div>
-        );
-    }
-}
-
-class BlueimpGallery extends React.Component {
+class MiniGallery extends React.Component {
     render() {
         return (
             <div className="MG_List MG_Auto">
@@ -56,35 +40,3 @@ class BlueimpGallery extends React.Component {
         );
     }
 }
-
-(function ($, window, document) {
-    $(() => {
-        ReactDOM.render(
-            <BlueimpGalleryLightbox />, $(".minigallery-lightbox").get(0)
-        );
-        var container = $(".minigallery-lightbox .blueimp-gallery").get(0);
-        $(".minigallery-inner").each ((i, m) => {
-            var moduleId = $(m).data ("module-id");
-            ReactDOM.render(
-                <BlueimpGallery
-                    ModuleId={moduleId}
-                    IsEditable={$(m).data ("is-editable")}
-                    EditIcon={$(m).data ("edit-icon")}
-                    Images={$(m).data ("images")}
-                />, m
-            );
-            $("a.MG_Link").click ((event) => {
-                var target = event.target || event.srcElement,
-                link = target.src ? target.parentNode : target,
-                options = {
-                    hidePageScrollbars: false,
-                    index: link,
-                    event: event,
-                    container: container
-                };
-                var links = $(target).closest(".minigallery-inner").find("a.MG_Link").get();
-                blueimp.Gallery(links, options);
-            });
-        });
-    });
-}) (jQuery, window, document);
