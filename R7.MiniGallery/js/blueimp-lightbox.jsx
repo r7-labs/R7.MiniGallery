58a05d4334a73f6ebf74b2dfcca01cc7eec96603
roman-yagodin <roman.yagodin@gmail.com>
2017-04-27 16:03:44 +0300

    Replace blueimp lightbox with jQuery version

--- R7.MiniGallery/js/blueimp-lightbox.jsx
+++ R7.MiniGallery/js/blueimp-lightbox.jsx
@@ -2,11 +2,11 @@ class BlueimpLightbox extends React.Component {
     render() {
         return (
             <div>
-                <div className={"blueimp-gallery" + ((this.props.showControls === true)? " blueimp-gallery-controls" : "")}>
+                <div id={this.props.gallery} className={"blueimp-gallery" + ((this.props.showControls === true)? " blueimp-gallery-controls" : "")}>
                     <div className="slides"></div>
                     <h3 className="title"></h3>
-                    <a className="prev">&lt;</a>
-                    <a className="next">&gt;</a>
+                    <a className="prev">&#8249;</a>
+                    <a className="next">&#8250;</a>
                     <a className="close">&times;</a>
                     <a className="play-pause"></a>
                     <ol className="indicator"></ol>
@@ -16,6 +16,7 @@ class BlueimpLightbox extends React.Component {
                     isEditable={this.props.isEditable}
                     editIcon={this.props.editIcon}
                     images={this.props.images}
+                    gallery={this.props.gallery}
                 />
             </div>
         );
@@ -33,23 +34,9 @@ class BlueimpLightbox extends React.Component {
                     editIcon={$(m).data ("edit-icon")}
                     images={$(m).data ("images")}
                     showControls={true}
+                    gallery={"gallery-" + moduleId}
                 />, m
             );
-
-            // TODO: Extract function, bind onclick statically
-            $("a.MG_Link").click ((event) => {
-                var target = event.target || event.srcElement;
-                var root = $(target).closest(".minigallery-root");
-                var link = target.src ? target.parentNode : target;
-                var options = {
-                    hidePageScrollbars: false,
-                    index: link,
-                    event: event,
-                    container: root.find (".blueimp-gallery").get(0)
-                };
-                var links = root.find("a.MG_Link").get();
-                blueimp.Gallery(links, options);
-            });
         });
     });
 }) (jQuery, window, document);
