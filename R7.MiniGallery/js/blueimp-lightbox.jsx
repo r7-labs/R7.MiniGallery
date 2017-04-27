43e33efb02cde834780e676e3415354c9e6ff946
roman-yagodin <roman.yagodin@gmail.com>
2017-04-27 15:10:09 +0300

    Add lightbox container for each gallery

--- R7.MiniGallery/js/blueimp-lightbox.jsx
+++ R7.MiniGallery/js/blueimp-lightbox.jsx
@@ -1,14 +1,22 @@
 class BlueimpLightbox extends React.Component {
     render() {
         return (
-            <div className="blueimp-gallery blueimp-gallery-controls">
-                <div className="slides"></div>
-                <h3 className="title"></h3>
-                <a className="prev">&lt;</a>
-                <a className="next">&gt;</a>
-                <a className="close">&times;</a>
-                <a className="play-pause"></a>
-                <ol className="indicator"></ol>
+            <div>
+                <div className={"blueimp-gallery" + ((this.props.showControls === true)? " blueimp-gallery-controls" : "")}>
+                    <div className="slides"></div>
+                    <h3 className="title"></h3>
+                    <a className="prev">&lt;</a>
+                    <a className="next">&gt;</a>
+                    <a className="close">&times;</a>
+                    <a className="play-pause"></a>
+                    <ol className="indicator"></ol>
+                </div>
+                <MiniGallery
+                    moduleId={this.props.moduleId}
+                    isEditable={this.props.isEditable}
+                    editIcon={this.props.editIcon}
+                    images={this.props.images}
+                />
             </div>
         );
     }
@@ -16,31 +24,30 @@ class BlueimpLightbox extends React.Component {
 
 (function ($, window, document) {
     $(() => {
-        ReactDOM.render(
-            <BlueimpLightbox />, $(".minigallery-lightbox").get(0)
-        );
-        var container = $(".minigallery-lightbox .blueimp-gallery").get(0);
         $(".minigallery-inner").each ((i, m) => {
             var moduleId = $(m).data ("module-id");
-            ReactDOM.render(
-                <MiniGallery
+            ReactDOM.render (
+                <BlueimpLightbox
                     moduleId={moduleId}
                     isEditable={$(m).data ("is-editable")}
                     editIcon={$(m).data ("edit-icon")}
                     images={$(m).data ("images")}
+                    showControls={true}
                 />, m
             );
+
             // TODO: Extract function, bind onclick statically
             $("a.MG_Link").click ((event) => {
-                var target = event.target || event.srcElement,
-                link = target.src ? target.parentNode : target,
-                options = {
+                var target = event.target || event.srcElement;
+                var root = $(target).closest(".minigallery-inner");
+                var link = target.src ? target.parentNode : target;
+                var options = {
                     hidePageScrollbars: false,
                     index: link,
                     event: event,
-                    container: container
+                    container: root.find (".blueimp-gallery").get(0)
                 };
-                var links = $(target).closest(".minigallery-inner").find("a.MG_Link").get();
+                var links = root.find("a.MG_Link").get();
                 blueimp.Gallery(links, options);
             });
         });
