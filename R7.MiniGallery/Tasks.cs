29df56a32c0fa9717fea94dbc989302fd30b3097
roman-yagodin <roman.yagodin@gmail.com>
2017-05-03 09:34:03 +0300

    Remove Tasks.cs

--- R7.MiniGallery/Tasks.cs
+++
@@ -1,9 +0,0 @@
-﻿// TODO: Update imagePreview then new file in urlImage is selected
-// TODO: Shorter setting prefix, e.g. MiniGallery_ => r7mg_
-// TODO: Add fancyBox2 support: http://fancyapps.com/fancybox/
-
-// REVIEW: How to display headers on images?
-// REVIEW: Simplify styleset names: MG_Default => mgDefault 
-// REVIEW: Don't use imagehandler if both width and height isn't set
-// REVIEW: Separate setting for image dimension units (same for width and height)
-// REVIEW: Display image title only on hover
