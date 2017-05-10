7332f51eb17dee54b55e628c6bc0902d2776a686
roman-yagodin <roman.yagodin@gmail.com>
2017-05-10 15:01:56 +0300

    Re-implement selecting styleset from dropdown. New styleset can be added by placing 'style-<stylename>.css' file into the module 'css' folder

---
+++ R7.MiniGallery/ViewModels/StyleSets.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+using DotNetNuke.Common;
+using R7.DotNetNuke.Extensions.TextExtensions;
+
+namespace R7.MiniGallery.ViewModels
+{
+    public static class StyleSets
+    {
+        public static IEnumerable<SelectListItem> Get (string selectedStyleSet)
+        {
+            return Directory.GetFiles (Path.Combine (Globals.ApplicationMapPath, "DesktopModules", "MVC", "R7.MiniGallery", "css"), "style-*.css").Select (file => {
+                var styleSet = Path.GetFileNameWithoutExtension (file).Substring (6).FirstCharToUpper ();
+                return new SelectListItem {
+                    Value = styleSet,
+                    Text = styleSet,
+                    Selected = styleSet == selectedStyleSet.ToString ()
+                };
+            });
+        }
+    }
+}
