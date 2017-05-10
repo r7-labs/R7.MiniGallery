345957d0827cbd5fe0acb881b31512f31f08cd82
roman-yagodin <roman.yagodin@gmail.com>
2017-05-10 14:15:13 +0300

    Re-implement selecting of lightbox type from dropdown

---
+++ R7.MiniGallery/ViewModels/Lightboxes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using DotNetNuke.Services.Localization;
+using R7.MiniGallery.Lightboxes;
+
+namespace R7.MiniGallery.ViewModels
+{
+    public static class Lightboxes
+    {
+        public static IEnumerable<SelectListItem> Get (LightboxType selectedLightbox, string localResourceFile)
+        {
+            return Enum.GetNames (typeof (LightboxType)).Select (lt => new SelectListItem {
+                Value = lt,
+                Text = Localization.GetString ("LightboxType." + lt, localResourceFile),
+                Selected = lt == selectedLightbox.ToString ()
+            });
+        }
+    }
+}
