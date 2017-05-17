00db1b7d45e0b12cacb3b338f1889c859d97283a
roman-yagodin <roman.yagodin@gmail.com>
2017-05-17 11:07:50 +0300

    No need to use array here

--- R7.MiniGallery/js/module-loader.jsx
+++ R7.MiniGallery/js/module-loader.jsx
@@ -20,22 +20,17 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (function ($, window, document) {
-    function getLightboxes () {
-        const lightboxes = [];
-        lightboxes ["Nonebox"] = MiniGallery;
-        lightboxes ["Lightbox"] = MiniGallery;
-        lightboxes ["Colorbox"] = MiniGallery;
-        if (typeof (BlueimpLightbox) !== "undefined") {
-            lightboxes ["BlueimpLightbox"] = BlueimpLightbox;
+    function getLightbox (lightboxType) {
+        if (lightboxType === "BlueimpLightbox" && typeof (BlueimpLightbox) !== "undefined") {
+            return BlueimpLightbox;
         }
-        return lightboxes;
+        return MiniGallery;
     }
 
     $(() => {
-        const lightboxes = getLightboxes ();
         $(".minigallery-root").each ((i, m) => {
             const moduleId = $(m).data ("module-id");
-            const Lightbox = lightboxes [$(m).data ("lightbox-type")];
+            const Lightbox = getLightbox ($(m).data ("lightbox-type"));
             ReactDOM.render (
                 <Lightbox
                     moduleId={moduleId}
