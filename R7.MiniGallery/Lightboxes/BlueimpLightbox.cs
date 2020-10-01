1c77bbf28abf68001c4ffb3957c1d4acc1d72844
roman-yagodin <roman.yagodin@gmail.com>
2020-10-01 10:06:36 +0400

    Load the highest version of blueimp Gallery CSS available

--- R7.MiniGallery/Lightboxes/BlueimpLightbox.cs
+++ R7.MiniGallery/Lightboxes/BlueimpLightbox.cs
@@ -1,27 +1,8 @@
-﻿//
-//  BlueimpLightbox.cs
-//
-//  Author:
-//       Roman M. Yagodin <roman.yagodin@gmail.com>
-//
-//  Copyright (c) 2017 Roman M. Yagodin
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-using System.Web.UI;
-using DotNetNuke.Framework.JavaScriptLibraries;
+﻿using System.Web.UI;
+using DotNetNuke.Web.Client;
 using DotNetNuke.Web.Client.ClientResourceManagement;
+using DotNetNuke.Framework.JavaScriptLibraries;
+using R7.Dnn.Extensions.Client;
 using R7.MiniGallery.Models;
 
 namespace R7.MiniGallery.Lightboxes
@@ -31,7 +12,11 @@ namespace R7.MiniGallery.Lightboxes
         public void Register (Page page)
         {
             JavaScript.RequestRegistration ("jQuery-BlueimpGallery");
-            ClientResourceManager.RegisterStyleSheet (page, "~/Resources/Libraries/jQuery-BlueimpGallery/02_25_00/css/blueimp-gallery.min.css");
+            var blueimpLibrary = JavaScriptLibraryHelper.GetHighestVersionLibrary ("jQuery-BlueimpGallery");
+            if (blueimpLibrary != null) {
+                JavaScriptLibraryHelper.RegisterStyleSheet (blueimpLibrary,
+                    page, "css/blueimp-gallery.min.css", (int) FileOrder.Css.SkinCss, "DnnPageHeaderProvider", "blueimp-gallery");
+            }
         }
 
         public string GetLinkAttributes (IImage image, int moduleId)
