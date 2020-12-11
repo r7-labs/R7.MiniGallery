2d7e6646c6821d3f322b2050e216fecd0e9e5e51
roman-yagodin <roman.yagodin@gmail.com>
2020-12-11 11:22:42 +0400

    Extract SolutionInfo.cs

---
+++ R7.MiniGallery/Properties/SolutionInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany ("R7.Labs")]
+[assembly: AssemblyProduct ("R7.MiniGallery")]
+[assembly: AssemblyCopyright ("Roman M. Yagodin")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyVersion ("1.5.2")]
+[assembly: AssemblyInformationalVersion ("1.5.2")]
