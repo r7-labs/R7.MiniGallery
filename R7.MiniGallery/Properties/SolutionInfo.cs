dbaffc8f29ca273fa5ed7079348338c684b751d1
roman-yagodin <roman.yagodin@gmail.com>
2020-12-16 10:24:01 +0400

    Bump version

--- R7.MiniGallery/Properties/SolutionInfo.cs
+++ R7.MiniGallery/Properties/SolutionInfo.cs
@@ -4,5 +4,5 @@ using System.Reflection;
 [assembly: AssemblyProduct ("R7.MiniGallery")]
 [assembly: AssemblyCopyright ("Roman M. Yagodin")]
 [assembly: AssemblyTrademark ("")]
-[assembly: AssemblyVersion ("1.5.2")]
-[assembly: AssemblyInformationalVersion ("1.5.2")]
+[assembly: AssemblyVersion ("1.6.0")]
+[assembly: AssemblyInformationalVersion ("1.6.0")]
